213f1343d163ba487274ea3e805a62c6d5cf688f
hayato.yoshida <hayato.yoshida@techacademy.jp>
2018-08-04 10:28:40 +0000

    third tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180731114550) do
+ActiveRecord::Schema.define(version: 20180804095901) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
