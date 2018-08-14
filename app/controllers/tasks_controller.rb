591c8e3664558bb513a4e2956c42bbbc15c1bca1
hayato.yoshida <hayato.yoshida@techacademy.jp>
2018-08-14 12:56:13 +0000

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,34 +1,58 @@
 class TasksController < ApplicationController
-
+  
+  def index
+    @tasks = Task.all.page(params[:page])
+  end
+  
   def new
+    @task = Task.new
   end
 
   def create
-    email = params[:session][:email].downcase
-    password = params[:session][:password]
-    if login(email, password)
-      flash[:success] = 'ログインに成功しました。'
-      redirect_to @user
+      @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'タスク が正常に投稿されました'
+      redirect_to @task
     else
-      flash.now[:danger] = 'ログインに失敗しました。'
-      render 'new'
+      flash.now[:danger] = 'タスク が投稿されませんでした'
+      render :new
     end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def destroy
+       @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'タスク は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+
+  if @task.update(task_params)
+      flash[:success] = 'タスク は正常に更新されました'
+      redirect_to @task
+  else
+      flash.now[:danger] = 'タスク は更新されませんでした'
+      render :edit
+    end
   end
 
-  private
+private
 
-  def login(email, password)
-    @user = User.find_by(email: email)
-    if @user && @user.authenticate(password)
-      # ログイン成功
-      session[:user_id] = @user.id
-      return true
-    else
-      # ログイン失敗
-      return false
-    end
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,11 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/destroy'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks
 
-  get 'login', to: 'tasks#new'
-  post 'login', to: 'tasks#create'
-  delete 'logout', to: 'tasks#destroy'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
